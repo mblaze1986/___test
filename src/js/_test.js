74835ecaf7abc2e5c870132d673aff1e5a69cc44
mblaze1986 <morty.blaze@gmail.com>
2021-04-30 16:21:03 +0300

    vertical tabs + fixes

--- src/js/_test.js
+++ src/js/_test.js
@@ -9,7 +9,7 @@ button.onclick = function () {
 
 
 
-// accordion with closing another tab ---------------------------
+// accordion with closing another tab ---------------------------------------------------
 
 
 $('.accordion__tab').click(function () {
@@ -20,14 +20,28 @@ $('.accordion__tab').click(function () {
 
 
 
-// tabs ----------------------------------
+// tabs -------------------------------------------------------------------------------------
 
+// horizontal
 
 $('.tabs').each(function () {
   let tabswitch = $(this);
   tabswitch.find('.tabs__item').not(':eq(1)').hide();
   tabswitch.find('.tabs__btn').click(function () {
-    tabswitch.find('.tabs__btn').removeClass('active').eq($(this).index()).addClass('active');
-    tabswitch.find('.tabs__item').hide(500).eq($(this).index()).show(500)
-  }).eq(1).addClass('active');
+    tabswitch.find('.tabs__btn').removeClass('tabs__btn--is-active').eq($(this).index()).addClass('tabs__btn--is-active');
+    tabswitch.find('.tabs__item').hide().eq($(this).index()).slideDown(500)
+  }).eq(1).addClass('tabs__btn--is-active');
+});
+
+
+
+// vertical
+
+$('.vertical-tabs').each(function () {
+  let tabswitch = $(this);
+  tabswitch.find('.vertical-tabs__item').not(':eq(2)').hide();
+  tabswitch.find('.vertical-tabs__btn').click(function () {
+    tabswitch.find('.vertical-tabs__btn').removeClass('vertical-tabs__btn--is-active').eq($(this).index()).addClass('vertical-tabs__btn--is-active');
+    tabswitch.find('.vertical-tabs__item').hide(500).eq($(this).index()).show(500)
+  }).eq(2).addClass('vertical-tabs__btn--is-active');
 });
